8cd2b50a236cb60f07473393c623271cb7372f42
johnofodile <63129718+johnofodile@users.noreply.github.com>
2023-04-09 17:15:27 +0200

    converted the displaying expense list to a dynamic list and created a filter effect for the dates

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm= () =>{
+const ExpenseForm= (props) =>{
 
     // const [userInput,setUserInput]= useState({
     //      enteredTitle:'',
@@ -53,7 +53,10 @@ const submitHandler=(event)=>{
         date: new Date(enteredDate)
 
     };
-    console.log(expenseData);
+props.onSaveExpenseData(expenseData);
+setEnteredAmount('');
+setEnteredTitle('');
+setEnteredDate('');
 
 };
 
@@ -62,19 +65,19 @@ return (
 <div className="new-expense__controls">
 <div className="new-expense__control">
     <label>Title</label>
-    <input type="text" onChange={titleChangeHandler}  />
+    <input type="text"  value={enteredTitle} onChange={titleChangeHandler}  />
 
 </div>
 <div className="new-expense__control">
     <label>Amount</label>
-    <input type="number" min="0.01" step=".01"   onChange={amountChangeHandler}    />
+    <input type="number" min="0.01" step=".01"  value={enteredAmount}  onChange={amountChangeHandler}    />
 
 </div>
 
 </div>
 <div className="new-expense__control">
     <label>Date</label>
-    <input type='date' min="2019-01-01"  max=" 2022-12-31"    onChange={dateChangeHandler}  />
+    <input type='date' min="2019-01-01"  max=" 2022-12-31"  value={enteredDate}  onChange={dateChangeHandler}  />
 
 </div>
 <div className="new-expense__actions">
